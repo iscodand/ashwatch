1a57c35eb1ef27ef041e2f8327d059bd3f647bb0
iscodandrade <isco.dev@outlook.com>
2026-02-19 08:52:22 -0300

    feature: define async log service contract

--- src/AshWatch.Application/Contracts/ILogService.cs
+++ src/AshWatch.Application/Contracts/ILogService.cs
@@ -1,9 +1,13 @@
+using AshWatch.Application.Dtos;
+using AshWatch.Application.Common;
+using AshWatch.Domain.Entities;
+
 namespace AshWatch.Application.Contracts;
 
 public interface ILogService
 {
-    void Log(string message);
-    void LogBatch(List<string> messages);
-    string GetLogById(int id);
-    List<string> GetAllLogs();
+    Task<DefaultResponse<Log>> LogAsync(CreateLogRequest request);
+    Task<DefaultResponse<List<Log>>> LogBatchAsync(List<CreateLogRequest> requests);
+    Task<DefaultResponse<Log>> GetLogByIdAsync(int id, int tenantId, int projectId);
+    Task<DefaultResponse<List<Log>>> GetAllLogsAsync(GetLogsFilterRequest request);
 }
