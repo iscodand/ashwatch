b7df47836b9f91aee1a59c4a67721b5bdb2e5be8
iscodandrade <isco.dev@outlook.com>
2026-02-19 08:52:22 -0300

    feature: add scoped log repository contract

--- src/AshWatch.Domain/Repositories/ILogRepository.cs
+++ src/AshWatch.Domain/Repositories/ILogRepository.cs
@@ -2,4 +2,8 @@ using AshWatch.Domain.Entities;
 
 namespace AshWatch.Domain.Repositories;
 
-public interface ILogRepository : IGenericRepository<Log> { }
+public interface ILogRepository : IGenericRepository<Log>
+{
+    Task<Log?> GetByIdAsync(int id, int tenantId, int projectId);
+    Task<IEnumerable<Log>> GetAllAsync(int tenantId, int projectId, DateTime? startDate, DateTime? endDate);
+}
