621b3713591a55401baf0de1795a692960d32cca
iscodandrade <isco.dev@outlook.com>
2026-02-19 08:52:22 -0300

    feature: wire logging endpoints and dependencies

--- src/AshWatch.Api/Program.cs
+++ src/AshWatch.Api/Program.cs
@@ -1,15 +1,22 @@
 using AshWatch.Application.Contracts;
+using AshWatch.Application.Dtos;
 using AshWatch.Application.Services;
+using AshWatch.Domain.Repositories;
+using AshWatch.Infrastructure.Data;
+using AshWatch.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ILogService, LogService>();
+builder.Services.AddScoped<ILogRepository, LogRepository>();
+
+builder.Services.AddSingleton<DataContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
-    config.DocumentName = "TodoAPI";
-    config.Title = "TodoAPI v1";
+    config.DocumentName = "AshWatch API";
+    config.Title = "AshWatch API v1";
     config.Version = "v1";
 });
 
@@ -24,12 +31,32 @@ app.UseSwaggerUi(config =>
     config.DocExpansion = "list";
 });
 
-app.MapPost("/logs", (string message) => $"Echo: {message}");
+app.MapPost("/logs", (CreateLogRequest request, ILogService logService) =>
+{
+    return logService.LogAsync(request);
+});
 
-app.MapPost("logs/batch", (List<string> messages) => $"Received {messages.Count} messages.");
+app.MapPost("/logs/batch", (List<CreateLogRequest> requests, ILogService logService) =>
+{
+    return logService.LogBatchAsync(requests);
+});
 
-app.MapGet("/logs/{id}", (int id) => $"Log ID: {id}");
+app.MapGet(
+    "/logs/{id}",
+    (int id, int tenantId, int projectId, ILogService logService) => logService.GetLogByIdAsync(id, tenantId, projectId)
+);
 
-app.MapGet("logs/", () => "Fetching all logs...");
+app.MapGet("/logs/", (int tenantId, int projectId, DateTime? startDate, DateTime? endDate, ILogService logService) =>
+{
+    var request = new GetLogsFilterRequest
+    {
+        TenantId = tenantId,
+        ProjectId = projectId,
+        StartDate = startDate,
+        EndDate = endDate
+    };
+
+    return logService.GetAllLogsAsync(request);
+});
 
 app.Run();
