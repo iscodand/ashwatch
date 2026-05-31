086545cdb10d2cbca094a99d6dec22f3c1f2b023
iscodandrade <isco.dev@outlook.com>
2026-05-31 17:19:57 -0300

    feature: enhance MongoDB connection handling and improve error reporting

--- ashwatch-worker/db.py
+++ ashwatch-worker/db.py
@@ -1,24 +1,43 @@
 import os
-
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-MONGO_URI=os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "ash_logging")
+MONGO_CA_FILE = os.getenv("MONGO_CA_FILE")
+
+_client = None
 
 
 def get_database():
+    global _client
+
     try:
-        client = MongoClient(MONGO_URI)
+        print(repr(MONGO_CA_FILE))
+        print(os.path.exists(MONGO_CA_FILE))
 
-        print("Connected to Mongo!")
+        if not os.path.exists(MONGO_CA_FILE):
+            raise Exception(f"CA file not found at: {MONGO_CA_FILE}")
 
-        return client["ash_logging_dev"]
+        if _client is None:
+            _client = MongoClient(
+                MONGO_URI,
+                tls=True,
+                tlsCAFile=MONGO_CA_FILE,
+                retryWrites=False,
+                serverSelectionTimeoutMS=5000,
+                tlsAllowInvalidHostnames=True
+            )
 
-    except ConnectionFailure as e:
-        print(f"Connection failure: {e}")
+            _client.admin.command("ping")
+            print("✅ Connected to Mongo (DocumentDB)!")
+
+        return _client[MONGO_DB_NAME]
+
+    except Exception as e:
+        print(f"❌ Error: {e}")
         return None
     
