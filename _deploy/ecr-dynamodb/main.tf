0ab33470651af44f7a47b04b151f83895827e522
iscodandrade <isco.dev@outlook.com>
2026-07-18 14:19:18 -0300

    feature: add Terraform configuration for ECR repositories and DynamoDB table

---
+++ _deploy/ecr-dynamodb/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "ashwatch-tf-state-bucket"
+    key          = "ecr-dynamodb/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "ashwatch-command-api" {
+  name                 = "ashwatch-command-api"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
+
+  tags = {
+    Name = "ashwatch-command-api"
+  }
+}
+
+resource "aws_ecr_repository" "ashwatch-query-api" {
+  name                 = "ashwatch-query-api"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
+
+  tags = {
+    Name = "ashwatch-query-api"
+  }
+}
+
+resource "aws_ecr_repository" "ashwatch-worker" {
+  name                 = "ashwatch-worker"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
+
+  tags = {
+    Name = "ashwatch-worker"
+  }
+}
+
+resource "aws_dynamodb_table" "ashwatch" {
+  name         = "ashwatch"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "ashwatch-dynamodb"
+  }
+}
