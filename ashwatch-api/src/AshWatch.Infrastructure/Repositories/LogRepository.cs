6e1d898b57276cc29b398d279cd53b80e11028e1
iscodandrade <isco.dev@outlook.com>
2026-07-27 22:55:45 -0300

    fix: remove Kafka integration and related producer and repository classes

--- ashwatch-api/src/AshWatch.Infrastructure/Repositories/LogRepository.cs
+++
@@ -1,9 +0,0 @@
-using AshWatch.Domain.Entities;
-using AshWatch.Domain.Repositories;
-using AshWatch.Infrastructure.Data;
-namespace AshWatch.Infrastructure.Repositories;
-
-public class LogRepository : GenericRepository<Log>, ILogRepository
-{
-    public LogRepository(ApplicationDbContext dataContext) : base(dataContext) { }
-}
